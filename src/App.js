d2d1bcf75ca2a9ab54e40aa6f82bcb5b41616a3e
heyitsabd <abdahmad0603@gmail.com>
2024-07-11 00:33:03 +0530

    Search funcationality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import users from './users.json';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredItems, setFilteredItems] = useState(users);
+
+  function handleDropdown() {
+    setShowDropdown(true);
+  }
+
+  function filterSearch(e) {
+    const value = e.target.value;
+    setInputValue(value);
+    const filtered = users.filter(user =>
+      user.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredItems(filtered);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={inputValue}
+        placeholder="Enter here"
+        onClick={handleDropdown}
+        onChange={filterSearch}
+      />
+      {showDropdown && filteredItems.length > 0 && (
+        <ul className="dropdown">
+          {filteredItems.map(({ name, id }) => (
+            <li onClick={()=>{setInputValue(name);setShowDropdown(false)}} key={id}>{name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
